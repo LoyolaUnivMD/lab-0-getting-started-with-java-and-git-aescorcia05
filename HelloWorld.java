c15044f9215787b8e6716932514ad85f5beaf748
aescorcia05 <156947111+aescorcia05@users.noreply.github.com>
2024-01-17 22:11:35 -0500

    I completed the Lab :)

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,27 @@
-//HelloWorld.java
+// Programmers:  Alejandro Escorcia
+// Course:  CS 212, Dr. Nweke
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: [Is your code based on an example in the book, in class, or something else?
 
 class HelloWorld {
     public static void main(String[] args) {
 
-        System.out.println("Hello, World!");
-        
+        System.out.println();
+        System.out.println("           ,ggg,      ,ggggggg,");
+        System.out.println("          dP\"\"8I    ,dP\"\"\"\"\"\"Y8b");
+        System.out.println("         dP   88    d8'    a  Y8");
+        System.out.println("        dP    88    88     \"Y8P'");
+        System.out.println("       ,8'    88    `8baaaa");
+        System.out.println("       d88888888   ,d8P\"\"\"\"");
+        System.out.println(" __   ,8\"     88   d8\"");
+        System.out.println("dP\"  ,8P      Y8   Y8,");
+        System.out.println("Yb,_,dP       `8b, `Yba,,_____,");
+        System.out.println(" \"Y8P\"         `Y8   `\"Y8888888");
+
+        // Credits: https://patorjk.com/software/taag/#p=display&f=NScript&t=Alejandro
     }
 }
