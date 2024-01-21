d8c48ded7b77d6cc62abe230e704ce72afcc2def
aescorcia05 <156947111+aescorcia05@users.noreply.github.com>
2024-01-21 16:25:16 -0500

    I completed the Lab :)

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,11 +5,12 @@
 // Problem Statement:  Output programmers’ initial
 // Input: None; there is no user input in this program
 // Output:  The initials of the programmers in ASCII art form
-// Credits: [Is your code based on an example in the book, in class, or something else?
+// Credits: https://patorjk.com/software/taag/#p=display&f=NScript&t=Alejandro
 
 class HelloWorld {
     public static void main(String[] args) {
 
+        System.out.println("This program will output it's creator's intials in ASCII art :)");
         System.out.println();
         System.out.println("           ,ggg,      ,ggggggg,");
         System.out.println("          dP\"\"8I    ,dP\"\"\"\"\"\"Y8b");
